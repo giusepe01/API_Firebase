aaeedfcca1c8d97c9bed42bc6a3319ec8be0bea2
giusepe01 <giusepe01@gmail.com>
2023-05-30 13:48:04 -0300

    first commit

---
+++ api/userAPI.py
@@ -0,0 +1,42 @@
+import uuid
+from flask import Blueprint, request, jsonify
+from firebase_admin import firestore
+
+db = firestore.client()
+user_Ref = db.collection('BeJpaD0QzVO7gdx86gP0w88rQ2D3')
+doc_Ref = db.collection('BeJpaD0QzVO7gdx86gP0w88rQ2D3').document('usE5jcmiLjHtxQNs95RX')
+
+userAPI = Blueprint('userAPI', __name__)
+
+@userAPI.route('/add', methods=['POST'])
+def create():
+        try:
+            id = uuid.uuid4()
+            user_Ref.document(id.hex).set(request.json)
+            return jsonify({"success": True}), 200
+        except Exception as e:
+            return f"An Error Occured: {e}"
+
+@userAPI.route('list', methods=['GET'])
+def read():
+     try:
+          all_devices = [doc.to_dict() for doc in user_Ref.stream()]
+          return jsonify(all_devices), 200
+     except Exception as e:
+          return f"An Error Occured: {e}"
+
+@userAPI.route('/att', methods=['PUT'])
+def update():
+        try:
+            doc_Ref.update(request.json)
+            return jsonify({"success": True}), 200
+        except Exception as e:
+            return f"An Error Occured: {e}"
+        
+@userAPI.route('single', methods=['GET'])
+def single():
+     try:
+          doc = doc_Ref.get()
+          return jsonify(doc.to_dict()), 200
+     except Exception as e:
+          return f"An Error Occured: {e}"
